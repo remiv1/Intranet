f707fb71e3c73659cb47dc57cdc9ac69c68256ea
remiv1 <verschuurrmm@outlook.fr>
2026-03-04 15:29:13 +0100

    fix: Corriger une erreur de syntaxe dans la requête de mise à jour des signatures

--- database/251003_MigrationScript.sql
+++ database/251003_MigrationScript.sql
@@ -8,7 +8,7 @@ STARTS CURRENT_TIMESTAMP
 DO
 BEGIN
     UPDATE 20_documents_a_signer
-    JOIN 21_points ON 21points.id_document = 20_documents_a_signer.id
+    JOIN 21_points ON 21_points.id_document = 20_documents_a_signer.id
     SET 21_points.status = -1,
         20_documents_a_signer.status = -1
     WHERE 20_documents_a_signer.limite_signature < CURRENT_DATE()
